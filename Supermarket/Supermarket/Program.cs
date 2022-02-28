e7d147487098831efe048b32604c88e2653fd31c
Fidel Augusto Villegas Arias <fidel.villegaz@gmail.com>
2022-02-27 23:48:52 -0500

    changes are added

--- Supermarket/Supermarket/Program.cs
+++ Supermarket/Supermarket/Program.cs
@@ -47,8 +47,8 @@ Console.WriteLine(product3);
 Console.WriteLine(product4);
 Console.WriteLine(product5);
 
-Invoice invoice = new Invoice();
-invoice.AddProduct(product1);
-invoice.AddProduct(product3);
-invoice.AddProduct(product5);
-Console.WriteLine(invoice);
+//Invoice invoice = new Invoice();
+//invoice.AddProduct(product1);
+//invoice.AddProduct(product3);
+//invoice.AddProduct(product5);
+//Console.WriteLine(invoice);
